3cfd12b052f5f20797d9ca1fb6cb749b46d99514
Laurent Kvb <laurent.kleeringvanbeerenbergh@leaseplan.com>
2024-03-26 15:20:54 +0100

    merge if-statements when no subsequent 'else' block exists

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,24 +23,19 @@ export class GildedRose {
         item.name != "Aged Brie" &&
         item.name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
+        if (item.name != "Sulfuras, Hand of Ragnaros" && item.quality > 0) {
+          item.quality = item.quality - 1;
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
+
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+            if (item.sellIn < 11 && item.quality < 50) {
+              item.quality = item.quality + 1;
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+            if (item.sellIn < 6 && item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
         }
@@ -49,21 +44,18 @@ export class GildedRose {
       if (item.name != "Sulfuras, Hand of Ragnaros") {
         item.sellIn = item.sellIn - 1;
       }
+
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
           if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
+            if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
+              item.quality = item.quality - 1;
             }
           } else {
             item.quality = item.quality - item.quality;
           }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+        } else if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       }
     }
